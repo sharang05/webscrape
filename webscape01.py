ffda56c485f036d4bc66d64ee382a410268ede81
sharang05 <sh.dwivedi82@gmail.com>
2018-06-16 08:46:28 +0530

    second commit

--- webscape01.py
+++ webscape01.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
+allUserAgents = []
+
 Chrome = requests.get('https://developers.whatismybrowser.com/useragents/explore/software_name/chrome/').text
 Firefox = requests.get('https://developers.whatismybrowser.com/useragents/explore/software_name/firefox/').text
 IE = requests.get('https://developers.whatismybrowser.com/useragents/explore/software_name/internet-explorer/').text
@@ -8,49 +10,30 @@ Opera = requests.get('https://developers.whatismybrowser.com/useragents/explore/
 Safari = requests.get('https://developers.whatismybrowser.com/useragents/explore/software_name/safari/').text
 UC = requests.get('https://developers.whatismybrowser.com/useragents/explore/software_name/uc-browser/').text
 
-browsers = [
-	"Chromesoup = BeautifulSoup(Chrome, 'lxml')",
-	"Firefoxsoup = BeautifulSoup(Firefox, 'lxml')", 
-	"IEsoup = 	 BeautifulSoup(IE, 'lxml')", 
-	"Operasoup  = BeautifulSoup(Opera, 'lxml')", 
-	"Safarisoup = BeautifulSoup(Safari, 'lxml')", 
-	"UCsoup =    BeautifulSoup(UC, 'lxml')"
 
-	]
-
-# Chromesoup = BeautifulSoup(Chrome, 'lxml')
-# Firefoxsoup = BeautifulSoup(Firefox, 'lxml')
-# IEsoup = 	 BeautifulSoup(IE, 'lxml')
-# Operasoup  = BeautifulSoup(Opera, 'lxml')
-# Safarisoup = BeautifulSoup(Safari, 'lxml')
-# UCsoup =    BeautifulSoup(UC, 'lxml')
+Chromesoup = BeautifulSoup(Chrome, 'lxml')
+Firefoxsoup = BeautifulSoup(Firefox, 'lxml')
+IEsoup = 	 BeautifulSoup(IE, 'lxml')
+Operasoup  = BeautifulSoup(Opera, 'lxml')
+Safarisoup = BeautifulSoup(Safari, 'lxml')
+UCsoup =    BeautifulSoup(UC, 'lxml')
 
 
 
-# for browser in browsers:
-# 	print(browser)
+userAgents = []
+for match in Chromesoup.find_all('td', class_='useragent'):
+	userAgent = match.a.text
+	userAgents.append(userAgent)
+allUserAgents.extend(userAgents[:4])
+
+for match in Firefoxsoup.find_all('td', class_='useragent'):
+	userAgent = match.a.text
+	userAgents.append(userAgent)
+allUserAgents.extend(userAgents[:4])
 
 
-UserAgents = []
+print(allUserAgents)
 
-for browser in browsers:
-	browserUserAgents = []
-	for match in browser.find_all('td', class_='useragent'):
-		browserUserAgent = match.a.text
-		browserUserAgents.append(browserUserAgent)
-	UserAgents.extend(browserUserAgents[:4])
-
-
-
-
-
-# userAgents = []
-# for match in Chromesoup.find_all('td', class_='useragent'):
-# 	userAgent = match.a.text
-# 	print(userAgent)
-# 	# userAgents.append(userAgent)
-
-# print(userAgents[:4])
 
 
 
